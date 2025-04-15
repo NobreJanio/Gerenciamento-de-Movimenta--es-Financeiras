027bc2772e4206f296e1ab457e17336f44c768b7
Jânio Alberto da Nóbrega Júnior <87720450+NobreJanio@users.noreply.github.com>
2025-04-15 05:09:43 -0300

    feat(dependencies): update package dependencies and enhance backend documentation
    Adicionadas novas dependências ao projeto, incluindo bibliotecas para exportação de arquivos Excel e PDF. Atualizada a documentação do backend para incluir instruções de configuração do ambiente e endpoints da API. Removido middleware CORS não utilizado e implementadas melhorias na exportação de transações, incluindo formatação e tratamento de erros. Além disso, foram feitas alterações na estrutura do frontend para otimizar a experiência do usuário.

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        'vue/setup-compiler-macros': true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        'plugin:prettier/recommended', // Integra o Prettier
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'prettier/prettier': 'warn', // Mostra warnings do Prettier como erros do ESLint
+        // Adicione ou sobrescreva regras aqui conforme necessário
+        // Exemplo: '@typescript-eslint/no-unused-vars': 'warn',
+    },
+}; 
